ee198548649ebcd09281eb0b931a3457079f3e81
Lucas Becker <lks.becker@gmail.com~>
2023-08-20 15:16:19 -0300

    create product handlers

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"net/http"
 
 	"github.com/LucBecker/imersao12-go-esquenta/internal/infra/akafka"
 	"github.com/LucBecker/imersao12-go-esquenta/internal/infra/repository"
+	"github.com/LucBecker/imersao12-go-esquenta/internal/infra/web"
 	"github.com/LucBecker/imersao12-go-esquenta/internal/usecase"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
@@ -17,11 +20,20 @@ func main() {
 	}
 	defer db.Close()
 
-	msgChan := make(chan *kafka.Message)
-	go akafka.Consume([]string{"products"}, "host.docker.internal:9094", msgChan)
-
 	repository := repository.NewProductRepositoryMysql(db)
 	createProductUsecase := usecase.NewCreateProductUseCase(repository)
+	listProductsUseCase := usecase.NewListProductsUseCase(repository)
+
+	productHandlers := web.NewProductHandlers(createProductUsecase, listProductsUseCase)
+
+	r := chi.NewRouter()
+	r.Post("/products", productHandlers.CreateProductHandler)
+	r.Get("/products", productHandlers.ListProductsHandler)
+
+	go http.ListenAndServe(":8000", r)
+
+	msgChan := make(chan *kafka.Message)
+	go akafka.Consume([]string{"products"}, "host.docker.internal:9094", msgChan)
 
 	for msg := range msgChan {
 		dto := usecase.CreateProductInputDto{}
