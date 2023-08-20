bead6ad33d9f639edcf5540fef876bd9c51aada6
Lucas Becker <lks.becker@gmail.com~>
2023-08-20 19:08:53 -0300

    create request http

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -11,10 +11,12 @@ import (
 	"github.com/LucBecker/imersao12-go-esquenta/internal/usecase"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/go-chi/chi/v5"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	db, err := sql.Open("mysql", "root:root@tcp(host.docker.internal:3306/products")
+	db, err := sql.Open("mysql", "root:root@tcp(host.docker.internal:3306)/products")
 	if err != nil {
 		panic(err)
 	}
